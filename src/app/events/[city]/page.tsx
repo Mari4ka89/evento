bd3781901b6938e529ffeff4e947d38fb5cea8ca
Mariia Bezrukava <mari4ka89@gmail.com>
2024-01-05 17:58:14 +0200

    Add Zod validation for Incoming data

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import { Metadata } from "next";
+import { z } from "zod";
 import H1 from "@/components/h1";
 import Loading from "./loading";
 import { capitalize } from "@/lib/utils";
@@ -19,19 +20,25 @@ export function generateMetadata({ params: { city } }: Props): Metadata {
   };
 }
 
+const pageNumberSchema = z.coerce.number().int().positive().optional();
+
 export default function EventsPage({
   params: { city },
   searchParams,
 }: EventsPageProps) {
-  const page = searchParams.page || 1;
+  const parsedPage = pageNumberSchema.safeParse(searchParams.page);
+
+  if (!parsedPage.success) {
+    throw new Error("Invalid page number");
+  }
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
         {city === "all" ? "All Events" : `Events is ${capitalize(city)}`}
       </H1>
-      <Suspense key={city + page} fallback={<Loading />}>
-        <EventsList city={city} page={+page} />
+      <Suspense key={city + parsedPage.data} fallback={<Loading />}>
+        <EventsList city={city} page={parsedPage.data} />
       </Suspense>
     </main>
   );
