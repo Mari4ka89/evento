f8b76a6543edccc4f534211038f17a138c94f46d
Mariia Bezrukava <mari4ka89@gmail.com>
2024-01-09 17:13:15 +0200

    Update DB to Postgres

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -2,12 +2,11 @@ import "server-only";
 import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
 import prisma from "./db";
-import { capitalize } from "./utils";
 
 export const getEvents = unstable_cache(async (city: string, page = 1) => {
   const events = await prisma.eventoEvent.findMany({
     where: {
-      city: city === "all" ? undefined : capitalize(city),
+      city: city === "all" ? undefined : city,
     },
     orderBy: {
       date: "asc",
@@ -22,7 +21,7 @@ export const getEvents = unstable_cache(async (city: string, page = 1) => {
   } else {
     totalCount = await prisma.eventoEvent.count({
       where: {
-        city: capitalize(city),
+        city,
       },
     });
   }
